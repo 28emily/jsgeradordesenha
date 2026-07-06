415bb145973f08633beb027821b17d7c283128db
28emily <fonseca.melo.emily@escola.pr.gov.br>
2026-07-06 12:25:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
+
+function diminuiTamanho(){ 
+
+}
